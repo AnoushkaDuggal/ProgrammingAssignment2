5374a72251f02430df247f10f50cf977fa1c6995
AnoushkaDuggal <aduggal2_be22@thapar.edu>
2024-10-28 14:30:01 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,47 @@
-##There are two functions makeCacheMatrix,makeCacheMatrix
-##makeCacheMatrix consists of set,get,setinv, getinv
-##library(MASS) is used to calculate inverse for non squared as well as square matrices
+# The 'MASS' package is used for generalized matrix inversions, which work for both square and non-square matrices
 library(MASS)
-makeCacheMatrix <- function(x = matrix()) {
-  inv<-NULL            #initializing inverse as NULL
-  set<-function(y){
-                  x<<-y
-                  inv<<-NULL
-                   }
-  get<-function()x             #function to get matrix x
-  setinv<-function(inverse)inv<<-inverse
-  getinv<-function(){ 
-                    inver<-ginv(x)
-                    inver%*%x           #function to obtain inverse of the matrix
-                    }
-  list(set = set, get = get, 
-       setinv = setinv, 
-       getinv = getinv)
+
+# Function to create a matrix object that can cache its inverse
+createCacheMatrix <- function(matrix = matrix()) {
+  inv_matrix <- NULL   # Initialize the inverse as NULL
+
+  # Function to set a new matrix
+  setMatrix <- function(new_matrix) {
+    matrix <<- new_matrix
+    inv_matrix <<- NULL  # Reset cached inverse when matrix changes
+  }
+
+  # Function to retrieve the current matrix
+  getMatrix <- function() matrix
+
+  # Function to set the inverse of the matrix
+  setInverse <- function(inverse) inv_matrix <<- inverse
+
+  # Function to retrieve the cached inverse, calculating if not available
+  getInverse <- function() {
+    if (is.null(inv_matrix)) {
+      inv_matrix <<- ginv(matrix)  # Compute the generalized inverse
+    }
+    inv_matrix %*% matrix  # Return the calculated inverse
+  }
+
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-##This is used to get the cache data
-
-cacheSolve <- function(x, ...) ##gets cache data      
-  {
-  inv<-x$getinv()                  
-  if(!is.null(inv)){                 #checking whether inverse is NUll 
-                     message("getting cached data!")
-                     return(inv)                       #returns inverse value
+# Function to compute or retrieve the cached inverse of a matrix
+computeCacheInverse <- function(matrix_obj, ...) {
+  inv_matrix <- matrix_obj$getInverse()  # Attempt to retrieve cached inverse
+  
+  # Check if the inverse is already cached
+  if (!is.null(inv_matrix)) {
+    message("Using cached inverse data!")
+    return(inv_matrix)  # Return the cached inverse
   }
-  data<-x$get()
-  inv<-solve(data,...)              #calculates inverse value
-  x$setinv(inv)
-  inv   ## Return a matrix that is the inverse of 'x'
+  
+  # If not cached, calculate the inverse
+  matrix_data <- matrix_obj$getMatrix()
+  inv_matrix <- solve(matrix_data, ...)  # Calculate the inverse using 'solve'
+  matrix_obj$setInverse(inv_matrix)      # Cache the newly computed inverse
+  inv_matrix   # Return the inverse
 }
